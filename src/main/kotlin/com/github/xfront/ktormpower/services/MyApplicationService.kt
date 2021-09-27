549e22e2ac31afa89bf5ea9e07058094e0e3cce2
GitHub Action <action@github.com>
2021-09-27 16:13:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/xfront/ktormpower/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xfront/ktormpower/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xfront.ktormpower.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xfront.ktormpower.MyBundle
 
 class MyApplicationService {
 
